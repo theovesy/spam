bda546164d269dfe9f946bdede29c5d9a465e361
theovesy <theovesy@tuta.io>
2023-04-14 16:24:15 +0200

    Subcommand cli!

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,60 @@
 mod spam;
-use clap::Parser;
+
+use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about=None)]
-struct Args {
-    #[arg(short, long)]
-    input_filename: String,
+#[command(name = "spam")]
+#[command(author, version, about, bin_name = "spam")]
+struct SpamCli {
+    #[command(subcommand)]
+    command: Commands,
+}
 
-    #[arg(short, long)]
-    output_filename: String,
+#[derive(Subcommand, Debug)]
+enum Commands {
+    #[command(arg_required_else_help = true)]
+    Make {
+        #[arg(short, long)]
+        input_filename: String,
 
-    #[arg(short, long, default_value_t = 1)]
-    resize_factor: u32,
+        #[arg(short, long)]
+        output_filename: String,
+
+        #[arg(short, long, default_value_t = 1)]
+        resize_factor: u32,
+    },
+    #[command(arg_required_else_help = true)]
+    Template {
+        #[arg(short, long)]
+        filename: String,
+
+        #[arg(short, long)]
+        width: u32,
+
+        #[arg(short='H', long)]
+        height: u32,
+    },
 }
 
 fn main() {
-    let args = Args::parse();
-    spam::process_file(&args.input_filename, 
-        &args.output_filename, args.resize_factor);
+    let args = SpamCli::parse();
+
+    match args.command { 
+        Commands::Make { 
+            input_filename,
+            output_filename,
+            resize_factor
+        } => {
+            spam::process_file(&input_filename, 
+                                &output_filename, 
+                                resize_factor);
+        }
+        Commands::Template {
+            filename,
+            width,
+            height,
+        } => {
+            spam::create_template(&filename, width, height);
+        }
+    } 
 }
